8223112ce3c2fb45ccb50ae0952ba6d893d86d4a
Honzapkcz <honzapkc@gmail.com>
2024-06-06 19:36:43 +0200

    Found out about Notification system.

--- TextWidget.gd
+++ TextWidget.gd
@@ -2,13 +2,16 @@ extends RefCounted
 ## Base class for use by TextWM
 class_name TextWidget
 
-signal redraw_request()
 signal mouse_move(position: Vector2, delta: Vector2)
 signal mouse_down(button: int)
 signal mouse_up(button: int)
 signal key_down(key: int)
 signal key_up(key: int)
 signal exit(code: int)
+
+const NOTIFICATION_DYNAMIC_PROCCESS: int = 3000
+const NOTIFICATION_STATIC_PROCCESS: int = 3001
+const NOTIFICATION_BUFFER_UPDATE: int = 3002
 
 var rect: Rect2i
 var parent: TextBuffer
@@ -20,6 +23,3 @@ var content: TextBuffer:
 
 func draw() -> void:
 	pass
-
-
-
