2309d43d62e85b5ffde8e2d78d2d28db32b9cbb2
Honzapkcz <honzapkc@gmail.com>
2024-06-06 17:44:31 +0200

    Enhanced TextWM initialization routine

--- TextWM.gd
+++ TextWM.gd
@@ -5,8 +5,10 @@ var j: int
 @export var width: int = 8
 @export var height: int = 8
 var font: RID
+var cursor: RID
 var canvas: RID
 var canvas_item: RID
+var mouse_position: Vector2
 var focus_window: TextWidget
 var windows: Array[TextWidget]
 var pallete: = PackedColorArray([Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK,
@@ -25,14 +27,12 @@ func get_textwidget(pos: Vector2i) -> TextWidget:
 	#return windows[0] # HACK
 	return null
 
-func _init() -> void:
+func _init(canvas_parent: RID) -> void:
 	font = load("res://itf.png")
+	cursor = load("res://cursor.png")
+	canvas = canvas_parent
 	canvas_item = RenderingServer.canvas_item_create()
-	call_deferred("post_init")
-
-func post_init() -> void:
 	RenderingServer.canvas_item_set_parent(canvas_item, canvas)
-	RenderingServer.canvas_item_set_draw_behind_parent(canvas_item, true)
 
 # TODO: need to fix this somehow
 func _draw() -> void:
@@ -50,6 +50,7 @@ func _draw() -> void:
 					int(buffer[(j * COLS + i) * 2] / 16) * height, width, height), pallete[buffer[(j * COLS + i) * 2 + 1] & 0b00001111])
 			i += 1
 		j += 1
+	RenderingServer.canvas_item_add_texture_rect(canvas_item, Rect2(Vector2(Vector2i(mouse_position)), Vector2(8, 14)), cursor)
 
 func _process(delta):
 	move(0, 0)
@@ -69,13 +70,15 @@ func _input(event: InputEvent):
 		return
 	if event is InputEventMouseButton:
 		if (event as InputEventMouseButton).pressed:
-			focus_window = get_textwidget((DisplayServer.mouse_get_position() - DisplayServer.window_get_position()) / 2)
+			focus_window = get_textwidget(mouse_position)
 			if not focus_window:
 				return
 			windows.push_front(windows.pop_at(windows.find(focus_window)))
 			focus_window.mouse_down.emit((event as InputEventMouseButton).button_index)
 		elif focus_window:
 			focus_window.mouse_up.emit((event as InputEventMouseButton).button_index)
-	elif event is InputEventMouseMotion and focus_window:
-		focus_window.mouse_move.emit(pos2char((event as InputEventMouseMotion).position), (event as InputEventMouseMotion).relative)
+	elif event is InputEventMouseMotion:
+		mouse_position = event.position
+		if focus_window:
+			focus_window.mouse_move.emit(pos2char((event as InputEventMouseMotion).position), (event as InputEventMouseMotion).relative)
 		
