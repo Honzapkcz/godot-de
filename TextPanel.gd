ca9d9a233c9f8ffab67efed23a1dbebff15e3890
Honzapkcz <honzapkc@gmail.com>
2024-06-07 19:24:37 +0200

    Invoke super() instread of duplicating code

--- TextPanel.gd
+++ TextPanel.gd
@@ -32,22 +32,7 @@ func draw():
 		child.copy(content, 1, 1)
 
 func set_rect(value: Rect2i) -> void:
-	if rect.size.x < 1:
-		push_error("rect.size.x cannot be less than 1")
-		rect.size.x = 1
-	if rect.size.y < 1:
-		push_error("rect.size.y cannot be less than 1")
-		rect.size.x = 1
-	if rect.position.x < 0:
-		push_error("rect.position.x cannot be less than 0")
-		rect.position.x = 0
-	if rect.position.y < 0:
-		push_error("rect.position.y cannot be less than 0")
-		rect.position.y = 0
-	if content:
-		content.COLS = value.size.x + 1
-		content.LINES = value.size.y + 1
+	super(value)
 	if child:
 		child.COLS = value.size.x + 1
 		child.LINES = value.size.y + 1
-	rect = value
