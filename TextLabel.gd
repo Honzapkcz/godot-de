f97c10a8a0dfaceee23af7f88421dcb63b5f2d72
Honzapkcz <honzapkc@gmail.com>
2024-06-07 18:02:00 +0200

    Implemented update logic. (draw routine missing)

--- TextLabel.gd
+++ TextLabel.gd
@@ -18,9 +18,46 @@ enum  {
 	#VERTICAL_ALIGNMENT_FILL,
 }
 
-var text: String
-var horizontal_align: int
-var vertical_align: int
-var visible_characters: int = -1
-var fg_color: int = 15
-var bg_color: int = 0
+var redraw: bool
+var text: String:
+	set(value):
+		text = value
+		redraw = true
+var horizontal_align: int:
+	set(value):
+		if value < 0 or value > 2:
+			push_error("horizontal_align out of range (" + str(value) + ")")
+			return
+		horizontal_align = value
+		redraw = true
+var vertical_align: int:
+	set(value):
+		if value < 0 or value > 2:
+			push_error("vertical_align out of range (" + str(value) + ")")
+			return
+		vertical_align = value
+		redraw = true
+var visible_characters: int = -1:
+	set(value):
+		visible_characters = value
+		redraw = true
+var fg_color: int = 15:
+	set(value):
+		fg_color = value
+		redraw = true
+var bg_color: int = 0:
+	set(value):
+		bg_color = value
+		redraw = true
+
+func _init():
+	content = TextBuffer.new()
+
+func _notification(what: int):
+	if what == NOTIFICATION_BUFFER_UPDATE and redraw:
+		draw()
+
+func draw() -> void:
+	content.buffer.size()
+	
+	redraw = false
