fd5937554533e177545046137e1384228f19778b
Honzapkcz <honzapkc@gmail.com>
2024-06-07 17:12:27 +0200

    Added peralacable setters & getters + values are not null on init.

--- TextWidget.gd
+++ TextWidget.gd
@@ -13,13 +13,37 @@ const NOTIFICATION_DYNAMIC_PROCCESS: int = 3000
 const NOTIFICATION_STATIC_PROCCESS: int = 3001
 const NOTIFICATION_BUFFER_UPDATE: int = 3002
 
-var rect: Rect2i
-var parent: TextBuffer
-var content: TextBuffer:
-	set(value):
-		value.COLS = rect.size.x - 1
-		value.LINES = rect.size.y - 1
-		content = value
+var rect: Rect2i = Rect2i(0, 0, 1, 1): set = set_rect, get = get_rect
+var content: TextBuffer = TextBuffer.new(): set = set_content, get = get_content
 
 func draw() -> void:
 	pass
+
+func set_rect(value: Rect2i) -> void:
+	if rect.size.x < 1:
+		push_error("rect.size.x cannot be less than 1")
+		rect.size.x = 1
+	if rect.size.y < 1:
+		push_error("rect.size.y cannot be less than 1")
+		rect.size.x = 1
+	if rect.position.x < 0:
+		push_error("rect.position.x cannot be less than 0")
+		rect.position.x = 0
+	if rect.position.y < 0:
+		push_error("rect.position.y cannot be less than 0")
+		rect.position.y = 0
+	if content:
+		content.COLS = value.size.x
+		content.LINES = value.size.y
+	rect = value
+
+func get_rect() -> Rect2i:
+	return rect
+
+func set_content(value: TextBuffer) -> void:
+	value.COLS = rect.size.x
+	value.LINES = rect.size.y
+	content = value
+
+func get_content() -> TextBuffer:
+	return content
