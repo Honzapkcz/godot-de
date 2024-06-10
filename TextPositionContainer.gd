044c3f5f3d27e98462368e826d1ddca4bd78c469
Honzapkcz <honzapkc@gmail.com>
2024-06-10 16:52:09 +0200

    Fixed two bugs, not redrawing on request & not clearing buffer

--- TextPositionContainer.gd
+++ TextPositionContainer.gd
@@ -4,17 +4,16 @@ class_name TextPositionContainer
 
 var children: Array[TextWidget]
 var i: int
-var child_redraw: bool
 
 func draw() -> void:
 	i = children.size() - 1
-	redraw = false
 	while i >= 0:
 		redraw = children[i].redraw or redraw
 		children[i].draw()
 		i -= 1
 	if not redraw:
 		return
+	content.clear(0x00)
 	i = children.size() - 1
 	while i >= 0:
 		children[i].content.copy(content, children[i].rect.position.x, children[i].rect.position.y)
